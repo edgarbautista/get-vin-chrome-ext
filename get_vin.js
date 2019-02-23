8640fd740f1bb97911e4bf4af74c57fedc1d0d76
ebautista3 <ebautista3@gatech.edu>
2019-02-23 00:46:06 -0500

    moving things around

--- get_vin.js
+++ get_vin.js
@@ -11,29 +11,36 @@ var httpStatus = 200;
 var popupCloseTimeInMs = 1000;
 
 var xhttp = new XMLHttpRequest();
-
-xhttp.onreadystatechange = function() {
-    if (this.readyState == readyState && this.status == httpStatus) {
-        var result = JSON.parse(this.responseText);
-        setWindowCopyVin(result);
-    }
+xhttp.request = function (requestVerb, url, async) {
+    this.open(requestVerb, url, async);
+    return this;
+};
+xhttp.configureResponse = function (responseHandler) {
+    xhttp.onreadystatechange = responseHandler;
+    return this;
 };
 
-function getVIN() {
-    xhttp.open(requestMethod, requestURL, true);
-    xhttp.send();
+function getVIN(responseHandler) {
+    xhttp.request(requestMethod, requestURL, true)
+        .configureResponse(responseHandler)
+        .send();
 }
 
 function setWindowCopyVin(result) {
     var vin = (result && result.vin) || errorMsg;
     setVINOnWindow(vin, inputElementId);
     copyTextFromElementById(inputElementId);
-    setTimeoutOfWindowClose(popupCloseTimeInMs);
+    setTimeoutWindowClose(popupCloseTimeInMs);
 }
 
 function setVINOnWindow(vin, id) {
-    var currentDiv = document.getElementById(id);
-    currentDiv.value = vin;
+    var currentDiv = null;
+    if (!!document) {
+        currentDiv = document.getElementById(id);
+    }
+    if (!!currentDiv) {
+        currentDiv.value = vin;
+    }
 }
 
 function copyTextFromElementById(id) {
@@ -44,10 +51,17 @@ function copyTextFromElementById(id) {
 
 }
 
-function setTimeoutOfWindowClose(time) {
+function setTimeoutWindowClose(time) {
     setTimeout(function () {
         window.close();
     }, time);
 }
 
-getVIN();
+var getVinResponseHandler = function() {
+    if (this.readyState == readyState && this.status == httpStatus) {
+        var result = JSON.parse(this.responseText);
+        setWindowCopyVin(result);
+    }
+};
+
+getVIN(getVinResponseHandler);
